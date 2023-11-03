30129002193c5628e6d28b7bdd415bc9a75c03c7
Arvin <webappsniper@gmail.com>
2023-11-03 15:36:16 +0800

    fix: 判断手机账号是否存在

--- public/class-jwt-auth-public.php
+++ public/class-jwt-auth-public.php
@@ -152,7 +152,7 @@ class Jwt_Auth_Public {
 			);
 
 			$users = get_users($args);
-			if ( $users[0] ) {
+			if ( $users && $users[0] ) {
 				$username = $users[0]->data->user_nicename;
 			}
 		}
